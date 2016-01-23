30164fc8a228d97e27301ced7591c77f8cae321d
Cristobal Mitchell <info@cristobalmitchell.com>
2016-01-23 12:52:55 -0600

    Basic Functionality
    Basic functionality added. Not all states have games listed.

--- Lottery_Numbers.py
+++ Lottery_Numbers.py
@@ -21,61 +21,69 @@ if debug:
 
 #GLOBAL DICTIONARIES
 usStates = {
-	"Alaska": "AK", 
-	"Alabama": "AL",
-	"Arkansas": "AR",
-	"Arizona": "AZ",
-	"California": "CA",
-	"Colorado": "CO",
-	"Connecticut": "CT",
-	"Delaware": "DE",
-	"Florida": "FL",
-	"Georgia": "GA",
-	"Hawaii": "HI",
-	"Iowa": "IA",
-	"Idaho": "ID",
-	"Illinois": "IL",
-	"Indiana": "IN",
-	"Kansas": "KS",
-	"Kentucky": "KY",
-	"Louisiana": "LA",
-	"Massachusetts": "MA",
-	"Maryland": "MD",
-	"Maine": "ME",
-	"Michigan": "MI",
-	"Minnesota": "MN",
-	"Missouri": "MO",
-	"Mississippi": "MS",
-	"Montana": "MT",
-	"North Carolina": "NC",
-	"North Dakota": "ND",
-	"Nebraska": "NE", 
-	"New Hampshire": "NH",
-	"New Jersey": "NJ",
-	"New Mexico": "NM",
-	"Nevada": "NV",
-	"New York": "NY",
-	"Ohio": "OH",
-	"Oklahoma": "OK",
-	"Oregon": "OR",
-	"Pennsylvania": "PA",
-	"Rhode Island": "RI",
-	"South Carolina": "SC", 
-	"South Dakota": "SD",
-	"Tennessee": "TN",
-	"Texas": "TX",
-	"Utah": "UT",
-	"Virginia": "VA",
-	"Vermont": "VT",
-	"Washington": "WA",
-	"Wisconsin": "WI",
-	"West Virginia": "WV",
-	"Wyoming": "WY"
+	"AK": ["","",""], 
+	"AL": ["","",""],
+	"AR": ["Powerball","Mega Millions"],
+	"AZ": ["Powerball","Mega Millions","Pick 3","Fantasy 5"],
+	"CA": ["Powerball","Mega Millions"],
+	"CO": ["Powerball","Mega Millions"],
+	"CT": ["Powerball","Mega Millions"],
+	"DE": ["Powerball","Mega Millions"],
+	"FL": ["Powerball","Mega Millions"],
+	"GA": ["Powerball","Mega Millions"],
+	"HI": ["","",""],
+	"IA": ["Powerball","Mega Millions"],
+	"ID": ["Powerball","Mega Millions"],
+	"IL": ["Powerball","Mega Millions","Pick 3","Pick 4","Lotto","Lucky Day Lotto"],
+	"IN": ["Powerball","Mega Millions"],
+	"KS": ["Powerball","Mega Millions"],
+	"KY": ["Powerball","Mega Millions"],
+	"LA": ["Powerball","Mega Millions"],
+	"MA": ["Powerball","Mega Millions"],
+	"MD": ["Powerball","Mega Millions"],
+	"ME": ["Powerball","Mega Millions"],
+	"MI": ["Powerball","Mega Millions"],
+	"MN": ["Powerball","Mega Millions"],
+	"MO": ["Powerball","Mega Millions"],
+	"MS": ["","",""],
+	"MT": ["Powerball","Mega Millions"],
+	"NC": ["Powerball","Mega Millions"],
+	"ND": ["Powerball","Mega Millions"],
+	"NE": ["Powerball","Mega Millions"], 
+	"NH": ["Powerball","Mega Millions"],
+	"NJ": ["Powerball","Mega Millions"],
+	"NM": ["Powerball","Mega Millions"],
+	"NV": ["Powerball","Mega Millions"],
+	"NY": ["Powerball","Mega Millions","Cash4Life","Numbers","Win 4"],
+	"OH": ["Powerball","Mega Millions"],
+	"OK": ["Powerball","Mega Millions"],
+	"OR": ["Powerball","Mega Millions"],
+	"PA": ["Powerball","Mega Millions"],
+	"RI": ["Powerball","Mega Millions"],
+	"SC": ["Powerball","Mega Millions"], 
+	"SD": ["Powerball","Mega Millions"],
+	"TN": ["Powerball","Mega Millions"],
+	"TX": ["Powerball","Mega Millions"],
+	"UT": ["","",""],
+	"VA": ["Powerball","Mega Millions"],
+	"VT": ["Powerball","Mega Millions"],
+	"WA": ["Powerball","Mega Millions"],
+	"WI": ["Powerball","Mega Millions"],
+	"WV": ["Powerball","Mega Millions"],
+	"WY": ["Powerball","Mega Millions"],
 }
 
 lotteryGames = {
-	"Powerball": ["","","","",""],
-	"Mega Millions": ["","","","","",""],
+	"Powerball": [5,69,1,26],
+	"Mega Millions": [5,75,1,15],
+	"Pick 3": [0,0,0,0,3,9],
+	"Pick 4": [0,0,0,0,4,9],
+	"Lotto": [6,45,0,0],
+	"Lucky Day Lotto": [5,45,0,0],
+	"Cash4Life": [5,60,1,4],
+	"Numbers": [0,0,0,0,3,9],
+	"Win 4": [0,0,0,0,4,9],
+	"Fantasy 5": [5,41,0,0]
 
 }
 
@@ -86,32 +94,55 @@ class Lottery:
 		print "\nStarting Lottery Numbers\n"
 
 	def stateSelector(self):
-		state = raw_input("Which state are you in? (You can use state initials or full state names)\n")
-		if (state in usStates) or (state.upper() in usStates.values()):
+		state = raw_input("Which state are you in? (Please use state initials)\n").upper()
+		if state.upper() in usStates:
 			self.gameSelector(state)
 		elif state == "exit":
-			exit(0)
+		 	exit(0)
 		else:
 			print "\nYou did not choose a valid state. Please try again.\n"
 			self.stateSelector()
 
 
 	def gameSelector(self,state):
-		print "You have selected the state of %s" % str(state)
-		gameToPlay = raw_input("Which game would you like to play?")
+		print "\nYou have selected the state of %s" % str(state)
+		gameToPlay = raw_input("\nWhich game would you like to play? (If you are unsure about what games are available type 'list')\n").title()
+		if gameToPlay == "List":
+			print
+			print usStates[state]
+			self.gameSelector(state)
+		if gameToPlay in usStates[state]:
+			print "Selected %s..." % gameToPlay
+			self.quickPick(state,gameToPlay)
 
 
-	def quickPick(self,game):
-		numbers = ""
-
-		return numbers
+	def quickPick(self,state,game):
+		numbers = []
+		if lotteryGames[game][0] != 0:
+			for i in range(lotteryGames[game][0]):
+				x = random.randint(1,lotteryGames[game][1])
+				if x not in numbers:
+					numbers.append(x)
+			numbers.sort()
+		if lotteryGames[game][2] != 0:
+			for i in range(lotteryGames[game][2]):
+				x = random.randint(1,lotteryGames[game][3])
+				numbers.append(x)		
+		try:
+			for i in range(lotteryGames[game][4]):
+				x = random.randint(0,lotteryGames[game][5])
+				numbers.append(x)
+		except IndexError:
+			pass
+		print numbers
+		self.writeToFile(state,game,numbers)
 
 
 	def writeToFile(self, state, game, numbers):
 		self.writeLocation = os.path.join(os.path.expanduser('~'), 'Desktop')
-		self.writeFilePath = os.path.join(self.writeLocation, usStates[state] + "_" + game + ".txt")
+		self.writeFilePath = os.path.join(self.writeLocation, state + "_" + game + ".txt")
 		self.writeFile = open(self.writeFilePath, "w")
-		self.writeFile.write(numbers)
+		self.writeFile.write(str(numbers))
 		self.writeFile.close()
 		if debug: 
 			print "%s %s file written to desktop" % (usStates[state],game)
---
+++ test.py
@@ -0,0 +1,7 @@
+lotteryGames = {
+	"Powerball": [5,69,1,26],
+	"Mega Millions": ["","","","","",""],
+
+}
+
+print lotteryGames["Powerball"][0]
